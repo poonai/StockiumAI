35da97206cf198ae5fc7e5c4c24b8bc5d5217755
root <rbalajis25@gmail.com>
2017-05-01 13:54:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from chatterbot import ChatBot
+from flask import jsonify,Flask,request
+chatbot = ChatBot(
+    'Anvena',
+    trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer',
+    storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+    database = 'chatbot'
+)
+chatbot.train('chatterbot.corpus.english')
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    q = request.args.get('q')
+    res = {
+    'response': str(chatbot.get_response(q))
+    }
+    return jsonify(res)
+if __name__ == '__main__':
+    app.run()
